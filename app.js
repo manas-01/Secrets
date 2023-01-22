dc199ce7396df4bb2a538cfe35bc89a6e59930e0
manas-01 <manasharsit@gmail.com>
2023-01-22 16:19:46 +0530

    added environment variables

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//jshint esversion:6
+require('dotenv').config();
 const express= require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -6,6 +6,7 @@ const mongoose= require("mongoose");
 const encrypt=require("mongoose-encryption");
 
 const app=express();
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -19,8 +20,7 @@ const userSchema = new mongoose.Schema( {
 });
 
 
-const secret="Thisismysecret";
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User= new mongoose.model("User",userSchema);
 
